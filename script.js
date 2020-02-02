f8d83e457b926d696300137d55c83a02bd2a8524
LiSplib <52524271+LiSplib@users.noreply.github.com>
2020-02-02 17:57:30 +0100

    modif visuelle

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const $postsList = document.getElementById('postsList');
+const removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
+const completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
 
 function updateView(){
     fetch('http://localhost:3000/api/v1/todos/?limit=300', {
@@ -17,16 +19,15 @@ function handleError(err){
 function readToDoPosts(data){
     let html = "";
     for(let toDo of data){
-                
+                     
         html +=`
-            <div class="card">
+            <div class="card my-2 rounded" id="addBorder">
                 <div class="card-body">
-                    <h5 class="card-title">${toDo.title}</h5>
-                    <p>Date de création du ToDO ${new Date(toDo.createdAt).toLocaleString()}</p>
-                    <p class="card-text">${toDo.id}</p>
-
-                    <div >
-                        <input dataCheck-id="${toDo.id}" type="checkbox" status="${toDo.done}" value="" id="defaultCheck${toDo.id}">
+                    <p>ToDO n° ${toDo.id} </p>
+                    <p class="text-right">Date de création: ${new Date(toDo.createdAt).toLocaleDateString()}</p>
+                    <h4 class="card-title bg-info">${toDo.title}</h4>
+                    <div class="text-left">
+                        <input dataCheck-id="${toDo.id}" type="checkbox" value="" id="defaultCheck${toDo.id}" ${stateCheckBox(toDo.done)}>
                         <label for="defaultCheck${toDo.id}">
                             Fait 
                         </label>
@@ -37,46 +38,49 @@ function readToDoPosts(data){
                     
                 </div>
             </div>
-            <div class="modal fade" id="exampleModal${toDo.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel${toDo.id}" aria-hidden="true">
+            
+    <!-- Modal Ouvert-->
+
+           <div class="modal fade" id="exampleModal${toDo.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel${toDo.id}" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel${toDo.id}">${toDo.title}</h5>
+                            <h5 class="modal-title bg-info" id="exampleModalLabel${toDo.id}">${toDo.title}</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                      <span aria-hidden="true">&times;</span>
                                 </button>
                         </div>
                         <div class="modal-body">
                             <p class="card-text">${toDo.content}</p>
-                            <p class="card-text">${toDo.id}</p>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-                            <button type="button" class="btn btn-warning" dataUpdate-id="${toDo.id}">Modifier</button>
-                            <button type="button" class="btn btn-danger" dataDelete-id="${toDo.id}">Effacer</button>
+                           <!-- <button type="button" class="btn btn-warning" dataUpdate-id="${toDo.id}">Modifier</button>
+                            <button type="button" class="btn btn-danger" dataDelete-id="${toDo.id}">Effacer</button> -->
                         </div>
                     </div>
                 </div>
             </div>
 
+    <!-- Modal de modification -->
             
             <div class="modal fade" id="updateModal${toDo.id}" tabindex="-1" role="dialog" aria-labelledby="updateModalLabel${toDo.id}" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="updateModalLabel${toDo.id}">Édition du ToDo ${toDo.id}</h5>
+                    <div class="modal-header align-center">
+                        <h5 class="modal-title" id="updateModalLabel${toDo.id}">Édition du ToDo n°${toDo.id}</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
                     <div class="modal-body">
                         <div class="form-group">
-                            <label for="title">${toDo.title}</label>
-                            <input type="text" class="form-control" id="exampleInputUpdateToDo${toDo.id}" name="titleUpdate" aria-describedby="TitleToDo" placeholder="Titre a modifier">
+                            <label for="title">Titre a modifier</label>
+                            <input type="text" class="form-control" id="exampleInputUpdateToDo${toDo.id}" name="titleUpdate" aria-describedby="TitleToDo" placeholder="${toDo.title}">
                         </div>
                         <div class="form-group">
-                            <label for="exampleInputUpdateContent${toDo.id}">${toDo.content}</label>
-                            <input type="text" class="form-control" id="contentUpdate${toDo.id}" name="contentUpdate" placeholder="Contenu a modidier">
+                            <label for="exampleInputUpdateContent${toDo.id}">Contenu a modidier</label>
+                            <input type="text" class="form-control" id="contentUpdate${toDo.id}" name="contentUpdate" placeholder="${toDo.content}">
                         </div>
                     </div>
                     <div class="modal-footer">
@@ -85,8 +89,9 @@ function readToDoPosts(data){
                     </div>
                 </div>
             </div>
-            </div>
+        </div>
 `;  
+
 }
 
 $postsList.innerHTML = html;
@@ -94,13 +99,16 @@ $postsList.innerHTML = html;
 
 //Récupère le bouton d'envoi et les contenus des inputs.
 
-const $createToDo = document.getElementById('toDo');
+const $createToDo = document.getElementById('toDoAdd');
 
 $createToDo.addEventListener('click', ev => { 
     let $titleToDo = document.getElementById('titleField').value;
     let $contentToDo = document.getElementById('contentField').value;
-    const data = {'title' : $titleToDo, 'content' : $contentToDo};
-    createToDo(data)
+        const data = {'title' : $titleToDo, 'content' : $contentToDo};
+        
+        createToDo(data);
+    document.getElementById('titleField').value = "";
+    document.getElementById('contentField').value = "";
 });
 
 function createToDo(data){
@@ -114,28 +122,22 @@ function createToDo(data){
     .then((response) => response.json())
     .then((result) => {
         console.log(result);
-
     })
     .catch((error) => {
         console.error('Error:', error);
     });
-
+  
 
 }
-//updateView(data);
-
-
-
 
 document.addEventListener('click', function (e) {
     const $target = e.target;
-    
+
     if ($target.hasAttribute("dataDelete-id")) {
         e.preventDefault();
         e.stopPropagation();
       const $id = Number($target.getAttribute("dataDelete-id"));
         deleteToDo($id);
-        // updateToDo($id);
     }
 });
 
@@ -156,6 +158,7 @@ function deleteToDo($id){
 document.addEventListener('click', function (e) {
     const $target = e.target;
     
+
     if ($target.hasAttribute("dataUpdate-id")) {
         e.preventDefault();
         e.stopPropagation();
@@ -184,24 +187,25 @@ function updateToDo($id, updateData){
         console.error('Error:', error);
     });
 
-
 }
 
 document.addEventListener('click', function (e) {
     const $target = e.target;
-    // let status = $target.getAttribute('value');
-    if ($target.hasAttribute("dataCheck-id") && status == "" || status == "checked") {
+    if ($target.hasAttribute("dataCheck-id") && !$target.hasAttribute('checked')) {
       let $id = Number($target.getAttribute('dataCheck-id'));
       let updateCheckData = {'done' : true};
-      $target.setAttribute("value", "checked");
-
+    //   let addBorder = document.getElementById("addBorder");
+    //   addBorder.classList.add("border-success");
       updateCheckToDo($id, updateCheckData);
-    }else{
+
+    }else {
+        $id = Number($target.getAttribute('dataCheck-id'));
         updateCheckData = {'done' : false};
         $target.setAttribute("value", "");
         updateCheckToDo($id, updateCheckData);
     }
 });
+
 function updateCheckToDo($id, updateCheckData){
     fetch(`http://localhost:3000/api/v1/todos/${$id}`, {
         method: "PATCH",
@@ -221,6 +225,12 @@ function updateCheckToDo($id, updateCheckData){
 
 
 }
-
+const stateCheckBox = (state) => {
+    if (state) {
+        return "checked";
+    } else {
+        return "";
+    }
+}
 
 updateView();
